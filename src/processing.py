4dde1211bec79f882ead67cb6a3d1c84335ebcbb
Daniil Yanyushevich <yanyushevich@icloud.com>
2025-03-09 22:12:48 +0300

    небольшие улучшения

--- src/processing.py
+++ src/processing.py
@@ -4,7 +4,7 @@ def filter_by_state(list_dictionaries: list, value: str = "EXECUTED") -> list:
     state соответствует указанному значению."""
     true_list = list()
     for dictionar in list_dictionaries:
-        if dictionar["state"] == value:
+        if dictionar.get("state") == value:
             true_list.append(dictionar)
     return true_list
 
@@ -13,6 +13,5 @@ def sort_by_date(list_dictionaries_: list, sorting_direction: bool = True) -> li
     """функция принимает список словарей и необязательный параметр, задающий порядок сортировки
     (по умолчанию — убывание) и  возвращать новый список, отсортированный по дате
     """
-    final_list = list()
     final_list = sorted(list_dictionaries_, key=lambda value: value["date"], reverse=sorting_direction)
     return final_list
