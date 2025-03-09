8c5fa93499e163f4f5bcac18b15eefcfb2ac3253
Daniil Yanyushevich <yanyushevich@icloud.com>
2025-03-09 14:00:07 +0300

    "добавил функцию sort_by_date в модуль processing.py"

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,11 @@ def filter_by_state(list_dictionaries:list, value:str = "EXECUTED") ->list:
         if dictionar['state'] == value:
             true_list.append(dictionar)
     return true_list
+
+def sort_by_date(list_dictionaries_:list,sorting_direction:bool = True) -> list:
+    """функция принимает список словарей и необязательный параметр, задающий порядок сортировки
+    (по умолчанию — убывание) и  возвращать новый список, отсортированный по дате
+"""
+    final_list = list()
+    final_list = sorted(list_dictionaries_, key = lambda value: value["date"], reverse = sorting_direction)
+    return final_list
